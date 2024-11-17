62b3be96a67937b4a9a2e799fc03f43ea65a1970
c2d <rgata.works@gmail.com>
2024-11-17 17:59:19 +0300

    mymaster: SendChatActionType

---
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/send/SendChatActionType.java
@@ -0,0 +1,13 @@
+package org.telegram.telegrambots.meta.api.methods.send;
+
+@SuppressWarnings("unused")
+public class SendChatActionType {
+    public static final String UPLOAD_PHOTO ="upload_photo";
+    public static final String RECORD_VIDEO ="record_video";
+    public static final String RECORD_VOICE ="record_voice";
+    public static final String UPLOAD_DOCUMENT ="upload_document";
+    public static final String CHOOSE_STICKER ="choose_sticker";
+    public static final String FIND_LOCATION ="find_location";
+    public static final String RECORD_VIDEO_NOTE ="record_video_note";
+    public static final String UPLOAD_VIDEO_NOTE ="upload_video_note";
+}
