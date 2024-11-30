1a424af9dbb4d1e616f220eb1b76a3ff8b299a38
c2d <rgata.works@gmail.com>
2024-11-30 17:40:03 +0300

    mymaster: script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mvn clean install -Dgpg.skip -DskipTests -Dmaven.javadoc.skip=true && \
-echo 1 && \
 find . -type d -name 'target' && \
 find . -type d -name 'target' -exec rm -rv {} +
